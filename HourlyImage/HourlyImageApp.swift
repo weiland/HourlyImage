a9c7a451826218a6cbb64230893513c66957a0f5
Pascal Weiland <weiland@users.noreply.github.com>
2022-03-30 19:39:49 +0200

    Initial commit :octocat:

---
+++ HourlyImage/HourlyImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyImageApp.swift
+//  HourlyImage
+//
+//  Created by Pascal on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct HourlyImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
